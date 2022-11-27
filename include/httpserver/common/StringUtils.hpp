ff9796f2c7a09bab12e922c0799a844147a67713
poicurr <ibot07092@gmail.com>
2022-11-28 00:23:36 +0900

    refine

--- include/httpserver/common/StringUtils.hpp
+++ include/httpserver/common/StringUtils.hpp
@@ -1,7 +1,21 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
 #include <vector>
+
+inline char toLower(char c) { return 'A' <= c && c <= 'Z' ? c + 'a' - 'A' : c; }
+inline char toUpper(char c) { return 'a' <= c && c <= 'z' ? c - 'a' - 'A' : c; }
+inline bool isAlpha(char c) {
+  return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z');
+}
+inline bool isNonZeroDigit(char c) { return '1' <= c && c <= '9'; }
+inline bool isDigit(char c) { return '0' == c || isNonZeroDigit(c); }
+inline bool isCR(char c) { return c == '\r'; }
+inline bool isLF(char c) { return c == '\n'; }
+inline bool isCrlf(char c) { return isCR(c) || isLF(c); }
+inline bool isSpace(char c) { return c == ' ' || c == '\t'; }
+inline bool isLetter(char c) { return isAlpha(c) || isDigit(c) || c == '_'; }
 
 std::vector<std::string> split(const std::string& s, const std::string& d) {
   if (s.empty()) return {};
@@ -25,3 +39,71 @@ bool endsWith(const std::string& str, const std::string& test) {
   return pos == str.size() - 1;
 }
 
+std::string toLower(const std::string& s) {
+  auto ret = std::string{};
+  for (const auto c : s) ret += toLower(c);
+  return ret;
+}
+
+std::string toUpper(const std::string& s) {
+  auto ret = std::string{};
+  for (const auto c : s) ret += toUpper(c);
+  return ret;
+}
+
+std::string trimLeft(const std::string& s) {
+  auto first = s.begin(), last = s.end();
+  while (isSpace(*first) && first != last) {
+    ++first;
+  }
+  return {first, last};
+}
+
+std::string trimRight(const std::string& s) {
+  auto first = s.begin(), last = first + s.size() - 1;
+  while (isSpace(*last) && first != last) {
+    --last;
+  }
+  return {first, last + 1};
+}
+
+std::string trim(const std::string& s) { return trimLeft(trimRight(s)); }
+
+std::string repeat(char c, size_t n) { return std::string(n, c); }
+
+std::string encodeURL(char c) {
+  static const char a[] = "0123456789ABCDEF";
+  auto ret = std::string{};
+  ret += a[(0xf0 & c) >> 4];
+  ret += a[0x0f & c];
+  return ret;
+}
+
+std::string encodeURL(const std::string& s) {
+  auto ret = std::string{};
+  for (char c : s) {
+    ret += "%" + encodeURL(c);
+  }
+  return ret;
+}
+
+char decode(const std::string& s) {
+  static const std::unordered_map<char, unsigned> m = {
+      {'0', 0},  {'1', 1},  {'2', 2},  {'3', 3},  {'4', 4},  {'5', 5},
+      {'6', 6},  {'7', 7},  {'8', 8},  {'9', 9},  {'A', 10}, {'B', 11},
+      {'C', 12}, {'D', 13}, {'E', 14}, {'F', 15},
+  };
+  uint8_t c1 = (m.at(s[0]) << 4);
+  uint8_t c2 = m.at(s[1]);
+  return static_cast<char>(c1 | c2);
+}
+
+std::string decodeURL(const std::string& s) {
+  auto v = split(s, "%");
+  auto ret = std::string{};
+  for (auto s : v) {
+    if (s.empty()) continue;
+    ret += decode(s);
+  }
+  return ret;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -6,50 +6,59 @@
 
 const int port = 8000;
 
+std::string load(std::ifstream& ifs) {
+  static char buffer[1024];
+  auto ret = std::string{};
+  while (!ifs.eof()) {
+    ifs.read(buffer, 1024);
+    ret.append(buffer, ifs.gcount());
+  }
+  return ret;
+}
+
+HttpResponse get(const HttpRequest& request) {
+  const auto path = request.header.path;
+  if (path.empty())
+    return HttpResponse{"HTTP/1.1 404 Not Found", "text/html", ""};
+  if (path == "/") {
+    auto target = "./resources/index.html";
+    std::ifstream ifs(target, std::ios::binary);
+    if (!ifs.is_open())
+      return HttpResponse{"HTTP/1.1 404 Not Found", "text/html", ""};
+    auto body = load(ifs);
+    return HttpResponse{"HTTP/1.1 200 OK", "text/html", body};
+  } else {
+    auto target = "./resources" + path;
+    std::ifstream ifs(target, std::ios::binary);
+    if (!ifs.is_open())
+      return HttpResponse{"HTTP/1.1 404 Not Found", "text/html", ""};
+    auto mimetype = std::string{"text/html"};
+    if (endsWith(target, ".ico")) {
+      mimetype = "image/x-icon";
+    } else if (endsWith(target, ".js")) {
+      mimetype = "application/javascript";
+    } else if (endsWith(target, ".css")) {
+      mimetype = "text/css";
+    }
+    auto body = load(ifs);
+    return HttpResponse{"HTTP/1.1 200 OK", mimetype, body};
+  }
+}
+
+HttpResponse post(const HttpRequest& request) {
+  const auto path = request.header.path;
+  return HttpResponse{"HTTP/1.1 404 Not Found", "text/html", ""};
+}
+
 int main(int argc, char const* argv[]) {
   std::cout << "Server running on http://localhost:" << port << std::endl;
   HttpServer server(port);
   server.run([](const HttpRequest& request) {
-    const auto path = request.header.path;
-    if (path.empty())
-      return HttpResponse{"HTTP/1.1 404 Not Found", "text/html", ""};
-
-    if (path == "/") {
-      auto target = "./resources/index.html";
-      std::ifstream ifs(target, std::ios::binary);
-      if (!ifs.is_open())
-        return HttpResponse{"HTTP/1.1 404 Not Found", "text/html", ""};
-
-      auto body = std::string{};
-      char buffer[1024];
-      while (!ifs.eof()) {
-        ifs.read(buffer, 1024);
-        body.append(buffer, ifs.gcount());
-      }
-      return HttpResponse{"HTTP/1.1 200 OK", "text/html", body};
-
-    } else {
-      auto target = "./resources" + path;
-      std::ifstream ifs(target, std::ios::binary);
-      if (!ifs.is_open())
-        return HttpResponse{"HTTP/1.1 404 Not Found", "text/html", ""};
-
-      auto mimetype = std::string{"text/html"};
-      if (endsWith(target, ".ico")) {
-        mimetype = "image/x-icon";
-      } else if (endsWith(target, ".js")) {
-        mimetype = "application/javascript";
-      } else if (endsWith(target, ".css")) {
-        mimetype = "text/css";
-      }
-
-      auto body = std::string{};
-      char buffer[1024];
-      while (!ifs.eof()) {
-        ifs.read(buffer, 1024);
-        body.append(buffer, ifs.gcount());
-      }
-      return HttpResponse{"HTTP/1.1 200 OK", mimetype, body};
-    }
+    const auto method = toUpper(request.header.method);
+    if (method == "GET")
+      return get(request);
+    else if (method == "POST")
+      return post(request);
+    return HttpResponse{"HTTP/1.1 404 Not Found", "text/html", ""};
   });
 }
